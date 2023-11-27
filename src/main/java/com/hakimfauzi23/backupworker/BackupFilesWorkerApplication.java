1896a774183c5f7501d312f8ee1de14dcbabd31a
Hanif <hanif.hakim@spesolution.com>
2023-11-27 10:15:15 +0700

    Init Project

---
+++ src/main/java/com/hakimfauzi23/backupworker/BackupFilesWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.hakimfauzi23.backupworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackupFilesWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackupFilesWorkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hakimfauzi23/backupworker/BackupFilesWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakimfauzi23.backupworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackupFilesWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
